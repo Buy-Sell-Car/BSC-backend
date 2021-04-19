b55d7437a39788f766dd95d7343bcdaa95bd88b2
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-19 17:08:10 +0300

    Add advert model

--- bsc/backend/models.py
+++ bsc/backend/models.py
@@ -11,3 +11,30 @@ class CarBrand(models.Model):
 class CarModel(models.Model):
     name = models.CharField(max_length=255)
     brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+
+
+class Advert(models.Model):
+
+    class Colors(models.TextChoices):
+        BLACK = 'BK', _('Black')
+        SILVER = 'SI', _('Silver')
+        WHITE = 'WH', _('White')
+        GREY = 'GY', _('Grey')
+        BLUE = 'BL', _('Blue')
+        RED = 'RE', _('Red')
+        GREEN = 'GN', _('Green')
+        BROWN = 'BR', _('Brown')
+        BEIGE = 'BE', _('Beige')
+        YELLOW = 'YE', _('Yellow')
+        PURPLE = 'PU', _('Purple')
+        PINK = 'PI', _('Pink')
+        Orange = 'OR', _('Orange')
+
+    carmodel = models.ForeignKey(CarModel, on_delete=models.PROTECT)
+    advert_date = models.DateField()
+    description = models.TextField()
+    price = models.IntegerField()
+    mileage = models.IntegerField()
+    prod_year = models.IntegerField()
+    owners = models.IntegerField()
+    color = models.CharField(choices=Colors.choices, max_length=2)
