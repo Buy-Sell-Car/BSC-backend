bf3d8ad907602e8f67488d9bcd613fa25d75c6d5
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-19 17:08:37 +0300

    Connect models to admin page

--- bsc/backend/admin.py
+++ bsc/backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CarBrand, CarModel, Advert
 
 # Register your models here.
+
+admin.site.register(CarBrand)
+admin.site.register(CarModel)
+admin.site.register(Advert)
