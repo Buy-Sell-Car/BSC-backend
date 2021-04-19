d83cb699489dad589dfa54077465afc8a986f8ef
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-19 17:07:41 +0300

    Add car models

--- bsc/backend/models.py
+++ bsc/backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class CarBrand(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class CarModel(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
