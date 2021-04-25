b34194a0df51508161dc3974e7cc03469df60db7
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-25 22:00:26 +0300

    Add owner permission

--- bsc/backend/permissions.py
+++ bsc/backend/permissions.py
@@ -14,3 +14,18 @@ class IsCurrentUserOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.id == request.user.id
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the advert.
+        return obj.profile.id == request.user.id
