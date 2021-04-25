fea4b16b18a1d13a3adaed9b923c66ef45157fd9
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-25 20:36:49 +0300

    Add profile view

--- bsc/backend/urls.py
+++ bsc/backend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import CarBrandAPIView, CarModelAPIView
+from .views import CarBrandAPIView, CarModelAPIView, ProfileAPIView
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'brands', CarBrandAPIView)
 router.register(r'models', CarModelAPIView)
+router.register(r'profile', ProfileAPIView)
 
 urlpatterns = [
     path('', include(router.urls)),
--- bsc/backend/views.py
+++ bsc/backend/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .models import CarBrand, CarModel
+from .models import CarBrand, CarModel, Profile
 
 from rest_framework import viewsets
-from .serializers import CarBrandSerializer, CarModelSerializer
+from .serializers import CarBrandSerializer, CarModelSerializer, ProfileSerializer
 
 
 class CarBrandAPIView(viewsets.ReadOnlyModelViewSet):
@@ -16,3 +16,8 @@ class CarBrandAPIView(viewsets.ReadOnlyModelViewSet):
 class CarModelAPIView(viewsets.ReadOnlyModelViewSet):
     queryset = CarModel.objects.all()
     serializer_class = CarModelSerializer
+
+
+class ProfileAPIView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
