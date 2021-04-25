945c205221eb90d8d9e5dc73e743483eeabee8da
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-25 18:17:49 +0300

    Add car views

--- bsc/backend/views.py
+++ bsc/backend/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import CarBrand, CarModel
+
+from rest_framework import viewsets
+from .serializers import CarBrandSerializer, CarModelSerializer
+
+
+class CarBrandAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = CarBrand.objects.all()
+    serializer_class = CarBrandSerializer
+
+
+class CarModelAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = CarModel.objects.all()
+    serializer_class = CarModelSerializer
