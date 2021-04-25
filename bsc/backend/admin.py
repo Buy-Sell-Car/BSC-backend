4b84ff275f9a2551850202cdb238e5befbad7af0
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-25 23:38:51 +0300

    Connect AdvertImage to admin page

--- bsc/backend/admin.py
+++ bsc/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CarBrand, CarModel, Advert, Profile
+from .models import CarBrand, CarModel, Advert, Profile, AdvertImage
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(CarBrand)
 admin.site.register(CarModel)
 admin.site.register(Advert)
 admin.site.register(Profile)
+admin.site.register(AdvertImage)
