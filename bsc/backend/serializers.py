28598048fa66fb773c5c63216c4e2daecea18f77
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-25 20:35:40 +0300

    Add profile serializer

--- bsc/backend/serializers.py
+++ bsc/backend/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import CarBrand, CarModel
+from .models import CarBrand, CarModel, Profile
 
 
 class CarBrandSerializer(serializers.ModelSerializer):
@@ -15,3 +15,24 @@ class CarModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarModel
         fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = Profile
+        fields = ('id', 'username', 'first_name', 'last_name',
+                  'email', 'password', 'city', 'tel')
