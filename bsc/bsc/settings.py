8fc5e407060338afa0b3bad9623c7ac294d8bcbf
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-27 23:20:04 +0300

    Add cors-headers for Vue

--- bsc/bsc/settings.py
+++ bsc/bsc/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'backend',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ORIGIN_ALLOW_ALL=True
