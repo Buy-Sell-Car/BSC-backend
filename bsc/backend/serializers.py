0b07477dc1ffd0a74c3ea9e743e003739473c76d
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-05-09 23:39:57 +0300

    Fix user update

--- bsc/backend/serializers.py
+++ bsc/backend/serializers.py
@@ -82,7 +82,8 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         user = super().update(instance, validated_data)
-        user.set_password(validated_data['password'])
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
         user.save()
         return user
 
